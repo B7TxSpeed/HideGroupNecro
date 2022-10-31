26c2f0fe42093ac89025229c2fcef80978e34939
B7TxSpeed <speed56400@gmail.com>
2022-10-31 18:23:42 +0100

    :bug: Fix packageAddon script for locales

--- packageAddon.sh
+++ packageAddon.sh
@@ -7,6 +7,8 @@ mkdir -p HideGroupNecro
 cp HideGroupNecro.txt HideGroupNecro/
 cp *.lua HideGroupNecro/
 cp *.xml HideGroupNecro/
+mkdir -p HideGroupNecro/locales
+cp locales/*.lua HideGroupNecro/locales/
 # Create ZIP and clean directory
 "C:\Program Files\7-Zip\7z" a -tzip HideGroupNecro-$version.zip HideGroupNecro
 rm -rf HideGroupNecro
